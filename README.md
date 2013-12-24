d1bde18ed30c293904f075c4fcd52513513329de
Matt Hughes <hughesm02@gmail.com>
2013-12-24 18:57:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# backbone-hotkeys
+
+A plugin for Backbone that extends `Backbone.View` to add more flexible hotkey-like bindings
+
+# Installation
+
+```
+npm install backbone-hotkeys
+```
---
+++ backbone-hotkeys.js
@@ -0,0 +1,10 @@
+// backbone-hotkeys 0.0.0
+//
+// (c) 2013 Matt Hughes
+// Licensed under the MIT license.
+
+(function(Backbone) {
+
+  // Your code goes here...
+
+}).call(this, Backbone);
