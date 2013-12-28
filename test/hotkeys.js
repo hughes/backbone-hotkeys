44b10491b1dcb0ebb7116bda0e7bbe1ea6b51885
Matt Hughes <hughesm02@gmail.com>
2013-12-27 22:41:47 -0500

    basic test to make sure it doesnt have script error

--- test/hotkeys.js
+++ test/hotkeys.js
@@ -1,5 +1,17 @@
 $(document).ready(function() {
 
-  module('Backbone.Hotkeys');
+    module('Backbone.Hotkeys');
+    Backbone.Hotkeys.on('all', function (e) { console.log(e); });
 
+    var MyHotkeyView = Backbone.Hotkeys.View.extend({
+        hotkeys: {
+            'a': 'a_btn_handler',
+            'ctrl+c': 'copy',
+            'ctrl+alt+f': function (key) { console.log('pressed', key); }
+        },
+        a_btn_handler: function (key) { console.log('pressed', key); },
+        copy: function () { console.log('copy'); }
+    });
+
+    var a = new MyHotkeyView();
 });
