d434d8c3074b57b3c149b7368b15aa225d98b667
Matt Hughes <hughesm02@gmail.com>
2013-12-27 22:56:14 -0500

    added some more keycodes

--- src/backbone-hotkeys.js
+++ src/backbone-hotkeys.js
@@ -28,13 +28,24 @@
     _.extend(Hotkeys, Backbone.Events);
 
     Hotkeys.CONST = {
+        backspace: 8,
+        tab: 9,
+        enter: 13,
         shift: 16,
         ctrl: 17,
         alt: 18,
+        caps: 20,
+        esc: 27,
+        pageup: 33,
+        pagedown: 34,
+        end: 35,
+        home: 36,
         left: 37,
         up: 38,
         right: 39,
-        down: 40
+        down: 40,
+        insert: 45,
+        delete: 46,
     };
 
     Hotkeys.translate = function (code) {
