55e3a4cbaf855647bbe7bde0fcbea4c67cb5bb69
Matt Hughes <hughesm02@gmail.com>
2013-12-27 21:57:10 -0500

    Added basic event system and stringmaking

--- src/backbone-hotkeys.js
+++ src/backbone-hotkeys.js
@@ -18,16 +18,82 @@
 }(function (_, Backbone) {
     'use strict';
 
-    var Hotkeys = {};
-    _.extend(Hotkeys, Backbone.Events);
+    var Hotkeys = {},
+        oldKeyDownHandler = document.onkeydown,
+        oldKeyUpHandler = document.onkeyup,
+        oldKeyPressHandler = document.onkeypress,
+        CONST,
+        translate;
 
-    Hotkeys.keyPressHandler = function (e) {
-        var pressed_char = String.fromCharCode(e.keyCode || e.charCode);
-        console.log(pressed_char);
+    CONST = {
+        shift: 16,
+        ctrl: 17,
+        alt: 18
     };
 
-    Backbone.Hotkeys = Hotkeys;
+    _.extend(Hotkeys, Backbone.Events);
+
+    translate = function (code) {
+        var names = _.keys(CONST),
+            codes = _.values(CONST),
+            index = codes.indexOf(code);
+
+        if (index > -1) {
+            // this key has a special name
+            return names[index];
+        } else {
+            // this key can just be converted to a string
+            return String.fromCharCode(code).toLocaleLowerCase();
+        }
+    };
+
+    Hotkeys.keyDownHandler = function (e) {
+        var key = translate(e.keyCode || e.which),
+            result = [];
+
+        // modifier keys
+        if (e.ctrlKey) {
+            result.push('ctrl');
+        }
+        if (e.altKey) {
+            result.push('alt');
+        }
+        if (e.shiftKey) {
+            result.push('shift');
+        }
+
+        result.push(key);
+
+        // remove duplicates (ctrl+ctrl)
+        result = _.uniq(result);
+
+        Hotkeys.trigger(result.join('+'));
+
+        if (_.isFunction(oldKeyDownHandler)) {
+            oldKeyDownHandler.call(this, e);
+        }
+        return;
+    };
+
+    Hotkeys.keyUpHandler = function (e) {
+        if (_.isFunction(oldKeyUpHandler)) {
+            oldKeyUpHandler.call(this, e);
+        }
+        return;
+    };
+
+    Hotkeys.keyPressHandler = function (e) {
+        if (_.isFunction(oldKeyPressHandler)) {
+            oldKeyPressHandler.call(this, e);
+        }
+        return;
+    };
+
+    document.onkeydown = Hotkeys.keyDownHandler;
+    document.onkeyup = Hotkeys.keyUpHandler;
     document.onkeypress = Hotkeys.keyPressHandler;
+
+    Backbone.Hotkeys = Hotkeys;
 
     return Backbone;
 }));
